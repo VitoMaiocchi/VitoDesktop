540ee7629cdef173db0d8aa1be3bd8e4d19d0087
Vito Maiocchi <vito.maiocchi@outlook.com>
2026-09-13 20:59:50 +0200

    PipeWire removed global variables

--- src/event_sources/pipewire.zig
+++ src/event_sources/pipewire.zig
@@ -15,119 +15,6 @@ const pipewire = @cImport({
 extern fn pw_init(argc: ?*c_int, argv: ?*?[*:0]u8) void;
 extern fn pw_deinit() void;
 
-var defaultAudioSink: ?[]const u8 = null;
-var nodes = std.StringHashMap(u32).init(std.heap.page_allocator);
-var defaultAudioSinkId: ?u32 = null;
-
-fn defaultCallback(
-    _: ?*anyopaque,
-    _: u32,
-    key: [*c]const u8,
-    _: [*c]const u8,
-    value: [*c]const u8,
-) callconv(.c) c_int {
-    if (std.mem.eql(u8, std.mem.span(key), "default.audio.sink")) {
-        const v = std.mem.span(value);
-
-        // v is JSON: {"name":"..."}
-        std.debug.print("default output = {s}\n", .{v});
-
-        const parsed = std.json.parseFromSlice(
-            struct { name: []const u8 },
-            std.heap.page_allocator,
-            v,
-            .{},
-        ) catch return 0;
-        defer parsed.deinit();
-
-        std.debug.print("default name = \"{s}\" \n", .{parsed.value.name});
-
-        defaultAudioSink = std.heap.page_allocator.dupe(u8, parsed.value.name) catch return 0;
-        if (defaultAudioSinkId == null) {
-            if (nodes.get(parsed.value.name)) |id| {
-                defaultAudioSinkId = id;
-                std.debug.print("DEFAULT SINK ID = {}\n", .{defaultAudioSinkId.?});
-            }
-        }
-    }
-
-    return 0;
-}
-
-var metadata_listener: pipewire.struct_spa_hook = undefined;
-
-var metadata_events = pipewire.struct_pw_metadata_events{
-    .version = pipewire.PW_VERSION_METADATA_EVENTS,
-    .property = defaultCallback,
-};
-
-fn registryCallback(
-    data: ?*anyopaque,
-    id: u32,
-    _: u32,
-    @"type": [*c]const u8,
-    _: u32,
-    props: [*c]const pipewire.struct_spa_dict,
-) callconv(.c) void {
-    if (std.mem.eql(
-        u8,
-        std.mem.span(@"type"),
-        pipewire.PW_TYPE_INTERFACE_Metadata,
-    )) {
-        var is_default = false;
-        if (props != null) {
-            const dict = props.*;
-            for (dict.items[0..dict.n_items]) |item| {
-                if (std.mem.eql(u8, std.mem.span(item.key), "metadata.name") and
-                    std.mem.eql(u8, std.mem.span(item.value), "default"))
-                {
-                    is_default = true;
-                }
-            }
-        }
-
-        if (!is_default) return;
-
-        const registry: *pipewire.struct_pw_registry = @ptrCast(@alignCast(data.?));
-        const metadata_ptr = pipewire.pw_registry_bind(
-            registry,
-            id,
-            pipewire.PW_TYPE_INTERFACE_Metadata,
-            pipewire.PW_VERSION_METADATA,
-            0,
-        );
-
-        if (metadata_ptr == null) return;
-
-        const metadata: *pipewire.struct_pw_metadata = @ptrCast(metadata_ptr.?);
-
-        _ = pipewire.pw_metadata_add_listener(metadata, &metadata_listener, &metadata_events, null);
-    }
-
-    if (std.mem.eql(u8, std.mem.span(@"type"), pipewire.PW_TYPE_INTERFACE_Node)) {
-        if (props == null) return;
-
-        const dict = props.*;
-        for (dict.items[0..dict.n_items]) |item| {
-            if (std.mem.eql(u8, std.mem.span(item.key), "node.name")) {
-                const node_name: []const u8 = std.mem.span(item.value);
-                std.debug.print("node: {s}, id: {}\n", .{ node_name, id });
-
-                if (defaultAudioSink != null) {
-                    if (std.mem.eql(u8, defaultAudioSink.?, node_name)) {
-                        defaultAudioSinkId = id;
-                        std.debug.print("DEFAULT SINK ID = {}\n", .{defaultAudioSinkId.?});
-                    }
-                }
-
-                const name_copy = std.heap.page_allocator.dupe(u8, node_name) catch return;
-
-                nodes.put(name_copy, id) catch return;
-            }
-        }
-    }
-}
-
 pub const PipeWire = struct {
     eventSource: *const EventSource,
     allocator: std.mem.Allocator,
@@ -138,10 +25,134 @@ pub const PipeWire = struct {
     pw_core: *pipewire.struct_pw_core,
     pw_registry: *pipewire.struct_pw_registry,
 
-    pw_registry_listener: pipewire.struct_spa_hook,
-    pw_registry_events: pipewire.struct_pw_registry_events,
+    pw_registry_listener: pipewire.struct_spa_hook = undefined,
+    pw_registry_events: pipewire.struct_pw_registry_events = .{
+        .version = pipewire.PW_VERSION_REGISTRY_EVENTS,
+        .global = registryCallback,
+    },
 
-    pub fn create(allocator: std.mem.Allocator) !*const PipeWire {
+    metadata_listener: pipewire.struct_spa_hook = undefined,
+    metadata_events: pipewire.struct_pw_metadata_events = .{
+        .version = pipewire.PW_VERSION_METADATA_EVENTS,
+        .property = defaultCallback,
+    },
+
+    defaultAudioSink: ?[]const u8 = null,
+    nodes: std.StringHashMap(u32),
+    defaultAudioSinkId: ?u32 = null,
+
+    fn defaultCallback(
+        data: ?*anyopaque,
+        _: u32,
+        key: [*c]const u8,
+        _: [*c]const u8,
+        value: [*c]const u8,
+    ) callconv(.c) c_int {
+        const self: *PipeWire = @ptrCast(@alignCast(data.?));
+
+        if (std.mem.eql(u8, std.mem.span(key), "default.audio.sink")) {
+            const v = std.mem.span(value);
+
+            // v is JSON: {"name":"..."}
+            std.debug.print("default output = {s}\n", .{v});
+
+            const parsed = std.json.parseFromSlice(
+                struct { name: []const u8 },
+                std.heap.page_allocator,
+                v,
+                .{},
+            ) catch return 0;
+            defer parsed.deinit();
+
+            std.debug.print("default name = \"{s}\" \n", .{parsed.value.name});
+
+            self.defaultAudioSink = std.heap.page_allocator.dupe(u8, parsed.value.name) catch return 0;
+            if (self.defaultAudioSinkId == null) {
+                if (self.nodes.get(parsed.value.name)) |id| {
+                    self.defaultAudioSinkId = id;
+                    std.debug.print("DEFAULT SINK ID = {}\n", .{self.defaultAudioSinkId.?});
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    fn registryCallback(
+        data: ?*anyopaque,
+        id: u32,
+        _: u32,
+        @"type": [*c]const u8,
+        _: u32,
+        props: [*c]const pipewire.struct_spa_dict,
+    ) callconv(.c) void {
+        const self: *PipeWire = @ptrCast(@alignCast(data.?));
+
+        if (std.mem.eql(
+            u8,
+            std.mem.span(@"type"),
+            pipewire.PW_TYPE_INTERFACE_Metadata,
+        )) {
+            var is_default = false;
+            if (props != null) {
+                const dict = props.*;
+                for (dict.items[0..dict.n_items]) |item| {
+                    if (std.mem.eql(u8, std.mem.span(item.key), "metadata.name") and
+                        std.mem.eql(u8, std.mem.span(item.value), "default"))
+                    {
+                        is_default = true;
+                    }
+                }
+            }
+
+            if (!is_default) return;
+
+            const registry = self.pw_registry;
+            const metadata_ptr = pipewire.pw_registry_bind(
+                registry,
+                id,
+                pipewire.PW_TYPE_INTERFACE_Metadata,
+                pipewire.PW_VERSION_METADATA,
+                0,
+            );
+
+            if (metadata_ptr == null) return;
+
+            const metadata: *pipewire.struct_pw_metadata = @ptrCast(metadata_ptr.?);
+
+            _ = pipewire.pw_metadata_add_listener(
+                metadata,
+                &self.metadata_listener,
+                &self.metadata_events,
+                self,
+            );
+        }
+
+        if (std.mem.eql(u8, std.mem.span(@"type"), pipewire.PW_TYPE_INTERFACE_Node)) {
+            if (props == null) return;
+
+            const dict = props.*;
+            for (dict.items[0..dict.n_items]) |item| {
+                if (std.mem.eql(u8, std.mem.span(item.key), "node.name")) {
+                    const node_name: []const u8 = std.mem.span(item.value);
+                    std.debug.print("node: {s}, id: {}\n", .{ node_name, id });
+
+                    if (self.defaultAudioSink != null) {
+                        if (std.mem.eql(u8, self.defaultAudioSink.?, node_name)) {
+                            self.defaultAudioSinkId = id;
+                            std.debug.print("DEFAULT SINK ID = {}\n", .{self.defaultAudioSinkId.?});
+                        }
+                    }
+
+                    const name_copy = std.heap.page_allocator.dupe(u8, node_name) catch return;
+
+                    self.nodes.put(name_copy, id) catch return;
+                }
+            }
+        }
+    }
+
+    pub fn create(allocator: std.mem.Allocator) !*PipeWire {
         pw_init(null, null);
 
         const pw_main_loop = pipewire.pw_main_loop_new(null);
@@ -173,24 +184,20 @@ pub const PipeWire = struct {
             .pw_context = pw_context.?,
             .pw_core = pw_core.?,
             .pw_registry = pw_registry.?,
-            .pw_registry_listener = undefined,
-            .pw_registry_events = .{
-                .version = pipewire.PW_VERSION_REGISTRY_EVENTS,
-                .global = registryCallback,
-            },
+            .nodes = std.StringHashMap(u32).init(allocator),
         };
 
         _ = pipewire.pw_registry_add_listener(
             pw_registry.?,
             &self.pw_registry_listener,
             &self.pw_registry_events,
-            pw_registry.?,
+            self,
         );
 
         eventSource.* = .{
             .fd = pipewire.pw_loop_get_fd(pw_loop.?),
             .events = std.posix.POLL.IN,
-            .dispatchFn = dispatchPipeWire,
+            .dispatchFn = dispatch,
             .context = pw_loop.?,
         };
 
@@ -209,7 +216,7 @@ pub const PipeWire = struct {
         self.allocator.destroy(self);
     }
 
-    fn dispatchPipeWire(event: *const EventSource) anyerror!void {
+    fn dispatch(event: *const EventSource) anyerror!void {
         if (event.context == null) return;
         const loop: *pipewire.struct_pw_loop = @ptrCast(@alignCast(event.context.?));
         _ = pipewire.pw_loop_iterate(loop, 0);
